22c2926f37bc90a5ea2448ba69f3d4863b8caeac
Dj. Aleksis <aleksis.exe@yandex.ru>
2022-04-19 18:43:12 +0500

    fix the release 0.0.1

--- src/app/shared/layouts/modules/header/components/logotype/logotype.component.ts
+++ src/app/shared/layouts/modules/header/components/logotype/logotype.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {environment} from 'src/environments/environment'
 
 @Component({
   selector: 'header-logotype',
   templateUrl: './logotype.component.html',
-  styleUrls: ['./logotype.component.scss']
+  styleUrls: ['./logotype.component.scss'],
 })
 export class LogotypeComponent implements OnInit {
-
-  constructor() { }
+  urlLogo: string = '/images'
+  constructor() {}
 
   ngOnInit(): void {
+    if (environment.production) {
+      this.urlLogo = '/app/admin-martynsystem/assets'
+    }
   }
-
 }
